68dc39e681fe4e8385338686ee14c8e8bf7da97b
Elizabeth Laricheva <li3ywa@gmail.com>
2026-09-14 13:38:11 +0300

    practic 1

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def check_room_available(is_available):
     return "Помещение занято"
 
 
-deff check_band_free(are_free):
+def check_band_free(are_free):
     if are_free:
         return "Все участники свободны"
     return "Не все участники свободны"
